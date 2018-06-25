22a897ffb247ceb4667a30ea59cce36467b596e8
silverbullet11 <gongming119@hotmail.com>
2018-06-25 08:26:20 -0700

    Add files via upload

--- pipelines.py
+++ pipelines.py
@@ -13,6 +13,9 @@ from sklearn.preprocessing import StandardScaler
 import collections
 from sklearn.metrics import accuracy_score
 
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline, FeatureUnion
+
 
 def restore_tokens_from_text(text):
     """
@@ -125,6 +128,109 @@ def convert_tokens_to_features(tokens, embedding_dim=100):
     return features
 
 
+class TokensPicker(BaseEstimator, TransformerMixin):
+
+    def __init__(self, embed_dim=10, top_n=10, window=5, min_count=1):
+        self.embedding_dim = embed_dim
+        self.top_n_token = top_n
+        self.window = window
+        self.min_count = min_count
+
+    def generate_word_embedding_from_df(self, df, col='tokens', vect_dims=100, window=5, min_count=1, workers=4, seed=11):
+        saved_model_name = w2v_file_name_from_parameters(col, vect_dims, window, min_count, seed)
+        if os.path.exists(saved_model_name):
+            return Word2Vec.load(saved_model_name)
+        else:
+            all_token_lists = df[col]
+            # size: The number of dimensions of the embedding, e.g. the length of the dense vector to represent each token(word)
+            # sg: THe training algorithm, either CBOW(0) or skip gram(1).
+            # window: The maximum distance between a target word and words around the target word.
+            # min_count: The minimum count of words to consider when training the model; words with an occurence less than this count will be ignored.
+            w2v_model = Word2Vec(sentences=all_token_lists, size=vect_dims, sg=1, window=window, min_count=min_count,
+                                 seed=seed, workers=workers)
+            w2v_model.save(saved_model_name)
+            return w2v_model
+
+    def fit(self, df, y=None):
+        self.w2v_model = self.generate_word_embedding_from_df(df,
+                                                    col='tokens',
+                                                    vect_dims=self.embedding_dim
+                                                    )
+        self.vectorizer = TfidfVectorizer()
+        self.vectors = self.vectorizer.fit_transform(df['sentence'].tolist())
+        return self
+
+    def get_top_tokens_in_doc(self, df, vectors, row_id, top_n=25):
+        row = np.squeeze(vectors[row_id].toarray())
+        tokens = df.loc[row_id]['tokens']
+        token_length = len(tokens)
+        #     print('Token length: ', str(token_length))
+        token_values = {}
+        for i in range(token_length):
+            # Get tfidf score for each token
+            token_name = tokens[i]
+            try:
+                if token_name in self.vectorizer.vocabulary_:
+                    token_index = self.vectorizer.vocabulary_[token_name]
+                    token_value = row[token_index]
+                else:
+                    token_value = 0
+            except:
+                print("Exception: ", str(row_id))
+            token_values[token_name] = token_value
+        # Sort the tokens by tfidf values
+        sorted_tokens = sorted(token_values.items(), key=operator.itemgetter(1), reverse=True)
+        #     print(sorted_tokens)
+        # Get the most weighted tokens
+        top_tokens = []
+        padding_count = 0
+        #     print("Sorted tokens length: ", str(len(sorted_tokens)))
+        if len(sorted_tokens) < top_n:
+            padding_count = top_n - len(sorted_tokens)
+            for i in range(len(sorted_tokens)):
+                top_tokens.append(sorted_tokens[i][0])
+        else:
+            for i in range(top_n):
+                top_tokens.append(sorted_tokens[i][0])
+        for i in range(padding_count):
+            top_tokens.append('UNKNOWN')
+        return top_tokens
+
+    def convert_tokens_to_features(self, tokens):
+        default_embedding = np.zeros(self.embedding_dim, dtype=int).tolist()
+        features = []
+        for t in tokens:
+            if t in self.w2v_model:
+                features += self.w2v_model[t].tolist()
+            else:
+                features += default_embedding
+        return features
+
+    def transform(self, df, y=None):
+        df['top_tokens'] = df.apply(lambda x: self.get_top_tokens_in_doc(df,
+                                                                       vectors=self.vectors,
+                                                                       row_id=x.name,
+                                                                       top_n=self.top_n_token),
+                                    axis=1
+                                  )
+        df['features'] = df['top_tokens'].apply(lambda x: self.convert_tokens_to_features(x))
+        df.to_csv('transformed_topn-{0}_embeddingdim-{1}.csv'.format(self.top_n_token, self.embedding_dim))
+        return df
+
+
+class DataFrameSlector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+
+    def fit(self, df, y=None):
+        return self
+
+    def transform(self, df):
+        # 根据传入的列明对DataFrame进行过滤
+        # 然后返回sklearn可以处理的数组数据
+        return df[self.attribute_names].values
+
+
 if __name__ == '__main__':
     training_path = '/home/alvin/!Final_Project/training_with_tokens.xlsx'
     testing_path = '/home/alvin/!Final_Project/testing_with_tokens.xlsx'
@@ -134,40 +240,11 @@ if __name__ == '__main__':
 
     print('Load data...')
     df_train = load_data(training_path)
-    print('Build word2vec model...')
-    w2v_model = generate_word_embedding_from_df(df_train,
-                                                col='tokens',
-                                                vect_dims=embedding_dim
-                                                )
-    print('Build TF-IDF vector...')
-    vectorizer = TfidfVectorizer()
-    vector = vectorizer.fit_transform(df_train['sentence'].tolist())
 
-    features = vectorizer.get_feature_names()
+    feature_pipeline = Pipeline([
+        ('Tokens_Picker_Pipeline', TokensPicker(embed_dim=5, top_n=5))
+    ])
 
-    print('Get top n tokens for each item...')
-    df_train['top_tokens'] = df_train.apply(
-                                                lambda x: get_top_tokens_in_doc(df_train,
-                                                                          Xtr=vector,
-                                                                          features=features,
-                                                                          row_id=x.name,
-                                                                          top_n=top_n_token),
-                                                axis=1
-                                            )
-
-    print('Create final features from top n tokens for each item...')
-    df_train['features'] = df_train['top_tokens'].apply(lambda x: convert_tokens_to_features(x, embedding_dim))
-
-    print('Build KMeans model...')
-    km = KMeans(n_clusters=11, init='k-means++', max_iter=100, n_init=1)
-    km.fit(df_train['features'].tolist())
-
-    print('Start to predict the results...')
-    df_train['predicted'] = km.predict(df_train['features'].tolist())
-
-    print('Save results to excel...')
-    df_train[['class', 'predicted', 'top_tokens']].to_csv('results.csv')
-
-    print('Accuracy score: ', accuracy_score(df_train['class'].tolist(), df_train['predicted'].tolist()))
+    x_train_transformed = feature_pipeline.fit_transform(df_train)
 
     print('Done...')
